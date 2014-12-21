38ff94708f0646ca11c11f006cc2cd945192a651
Leandro Vázquez Cervantes <leandro.vazquez@devtopia.coop>
2014-12-21 21:57:23 +0100

    not override 4 favs for normal mode when going back from mini mode

--- js/homecake.js
+++ js/homecake.js
@@ -66,7 +66,7 @@
                 break;
             case 1:
                 //favs for normal mode
-                favs = ['Camera','Settings','Phone','Music'];
+                if (favs.length > 4) favs = favs.slice(2);
 
                 iconsize = 64;
                 if (bottom) show_bottom();
